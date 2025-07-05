de2f8be4f912e01eba213d0a85894084a83fda63
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-07-06 00:25:36 +0900

    chore: 절대경로 지정 (@ = src)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') },
-      { find: '@pages', replacement: resolve(__dirname, 'src/pages') },
-    ],
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
 });
