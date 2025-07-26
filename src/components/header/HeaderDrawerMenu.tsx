25c23e1bfcefb4634b6b0a913166a24f911986a5
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-07-26 21:08:17 +0900

    feat: mailto 적용

--- src/components/header/HeaderDrawerMenu.tsx
+++ src/components/header/HeaderDrawerMenu.tsx
@@ -8,12 +8,14 @@ const HeaderDrawerMenu: React.FC<HeaderDrawerMenuProps> = ({ open }) => {
   return (
     <>
       <div
-        className={`border-ourGray fixed w-full overflow-hidden bg-white transition-all duration-500 sm:hidden ${open ? 'fixed top-15 max-h-48 translate-y-0' : 'max-h-0 -translate-y-2'}`}
+        className={`border-ourGray fixed z-99 w-full overflow-hidden bg-white transition-all duration-500 sm:hidden ${open ? 'fixed top-15 max-h-48 translate-y-0' : 'max-h-0 -translate-y-2'}`}
       >
         <ul className="text-ourTextBlack">
           <li className="c-drawer-menu-item">시작하기</li>
           <li className="c-drawer-menu-item">다운로드</li>
-          <li className="c-drawer-menu-item">문의하기</li>
+          <li className="c-drawer-menu-item">
+            <a href="mailto:dhtmxk8134@naver.com">문의하기</a>
+          </li>
         </ul>
       </div>
     </>
