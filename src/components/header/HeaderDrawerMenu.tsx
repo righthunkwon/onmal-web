6b5d3943d5acf27a75c06c9d76f93e920c5ea5e6
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-07-27 20:50:56 +0900

    feat: 메뉴 라우터 추가

--- src/components/header/HeaderDrawerMenu.tsx
+++ src/components/header/HeaderDrawerMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface HeaderDrawerMenuProps {
   open: boolean;
@@ -11,10 +12,20 @@ const HeaderDrawerMenu: React.FC<HeaderDrawerMenuProps> = ({ open }) => {
         className={`border-ourGray fixed z-99 w-full overflow-hidden bg-white transition-all duration-500 sm:hidden ${open ? 'fixed top-15 max-h-48 translate-y-0' : 'max-h-0 -translate-y-2'}`}
       >
         <ul className="text-ourTextBlack">
-          <li className="c-drawer-menu-item">시작하기</li>
-          <li className="c-drawer-menu-item">다운로드</li>
           <li className="c-drawer-menu-item">
-            <a href="mailto:dhtmxk8134@naver.com">문의하기</a>
+            <Link className="block w-full" to={'/start'}>
+              시작하기
+            </Link>
+          </li>
+          <li className="c-drawer-menu-item">
+            <Link className="block w-full" to={'/download'}>
+              다운로드
+            </Link>
+          </li>
+          <li className="c-drawer-menu-item">
+            <Link to="mailto:dhtmxk8134@naver.com" className="block w-full">
+              문의하기
+            </Link>
           </li>
         </ul>
       </div>
--- src/components/header/HeaderNavbarMenu.tsx
+++ src/components/header/HeaderNavbarMenu.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HeaderNavbarMenu: React.FC = () => {
   return (
     <>
-      <ul className="text-ourTextBlack hidden sm:flex">
-        <li className="c-nav-menu-item">시작하기</li> {/* 시작하기 */}
-        <li className="c-nav-menu-item">다운로드</li> {/* 다운로드 */}
-        <li className="c-nav-menu-item">
-          <a href="mailto:dhtmxk8134@naver.com">문의하기</a> {/* 문의하기 */}
+      <ul className="text-ourTextBlack my-auto hidden sm:flex">
+        <li>
+          {/* 시작하기 */}
+          <Link className="c-nav-menu-item" to={'/start'}>
+            시작하기
+          </Link>
+        </li>
+        <li>
+          {/* 다운로드 */}
+          <Link className="c-nav-menu-item" to={'/download'}>
+            다운로드
+          </Link>
+        </li>
+        <li>
+          {/* 문의하기 */}
+          <Link className="c-nav-menu-item" to="mailto:dhtmxk8134@naver.com">
+            문의하기
+          </Link>
         </li>
       </ul>
       <button className="border-ourBlue bg-ourBlue mx-2 my-auto hidden h-fit cursor-pointer rounded-lg border-1 px-1.5 py-1 whitespace-nowrap text-white sm:flex">
