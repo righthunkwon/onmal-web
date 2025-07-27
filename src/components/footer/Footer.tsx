e6236149a5b4371fc68c28747b0d44989d771912
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-07-27 20:50:40 +0900

    feat: 푸터 노션 링크 추가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import github from '@/assets/svgs/btn-github.svg';
 import email from '@/assets/svgs/btn-email.svg';
+import notion from '@/assets/svgs/btn-notion.svg';
 
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className="c-footer bg-ourDarkGray p-8 text-sm text-white sm:p-12 sm:text-base">
+      <footer className="p-8 text-sm text-white c-footer bg-ourDarkGray sm:p-12 sm:text-base">
         <div className="mx-auto max-w-[1140px] sm:p-4">
           <ul className="pb-8 pl-1">
             <li className="mb-3 font-bold">서비스</li>
             <li className="py-2">
               <a
-                className="text-ourTextLightGray py-2 pr-4 hover:brightness-80"
+                className="py-2 pr-4 text-ourTextLightGray hover:brightness-80"
                 href="#introduce"
               >
                 소개
@@ -19,36 +21,50 @@ const Footer: React.FC = () => {
             </li>
 
             <li className="py-2">
-              <a
-                className="text-ourTextLightGray py-2 pr-4 hover:brightness-80"
-                href="#시작하기페이지로이동"
+              <Link
+                className="py-2 pr-4 text-ourTextLightGray hover:brightness-80"
+                to={'/start'}
               >
                 시작하기
-              </a>
+              </Link>
             </li>
             <li className="py-2">
-              <a
-                className="text-ourTextLightGray py-2 pr-4 hover:brightness-80"
-                href="#다운로드페이지로이동"
+              <Link
+                className="py-2 pr-4 text-ourTextLightGray hover:brightness-80"
+                to={'/download'}
               >
                 다운로드
-              </a>
+              </Link>
             </li>
           </ul>
           <ul className="flex gap-2">
-            <li className="h-12 w-12">
-              <a href="https://github.com/righthunkwon" target="_blank">
+            <li className="w-12 h-12">
+              <Link to={'https://github.com/righthunkwon'} target="_blank">
                 <img
                   className="hover:brightness-90"
                   src={github}
                   alt="github"
                 />
-              </a>
+              </Link>
             </li>
-            <li className="h-12 w-12">
-              <a href="mailto:dhtmxk8134@naver.com">
+            <li className="w-12 h-12">
+              <Link
+                to={
+                  'https://www.notion.so/righthun/dd055ee9f54445e89cd84d755071f7bd'
+                }
+                target="_blank"
+              >
+                <img
+                  className="p-1 hover:brightness-90"
+                  src={notion}
+                  alt="notion"
+                />
+              </Link>
+            </li>
+            <li className="w-12 h-12">
+              <Link to={'mailto:dhtmxk8134@naver.com'}>
                 <img className="hover:brightness-90" src={email} alt="email" />
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
