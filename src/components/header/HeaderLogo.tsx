b53679ac01b0a925a1ffa849bcf14c255b19ee5a
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-07-27 20:51:06 +0900

    feat: 로고 홈 라우터 추가

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from '@/assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const HeaderLogo: React.FC = () => {
   return (
     <>
-      <div className="mx-2 flex cursor-pointer items-center px-3 py-2">
-        <img className="mx-2 h-6 w-6" src={logo} alt="logo" />
-        <span className="text- text-xl font-semibold">온말</span>
-      </div>
+      <Link
+        to={'/'}
+        className="flex cursor-pointer items-center justify-center gap-1 px-6 py-2 sm:ml-6"
+      >
+        <img className="h-6 w-6" src={logo} alt="logo" />
+        <span className="text-xl font-semibold">온말</span>
+      </Link>
     </>
   );
 };
