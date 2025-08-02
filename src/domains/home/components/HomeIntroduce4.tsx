53695384b0375df55512cbc95e0576b494bd855c
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2025-08-02 20:20:25 +0900

    style: 홈 링크 버튼 디자인 변경

--- src/domains/home/components/HomeIntroduce4.tsx
+++ src/domains/home/components/HomeIntroduce4.tsx
@@ -14,30 +14,40 @@ const HomeIntroduce4: React.FC = () => {
           <div className="flex flex-1 flex-col gap-4">
             <div className="rounded-lg bg-neutral-950 px-8 py-12 sm:px-16">
               <div className="mb-1 text-2xl font-bold">온말 표준데이터</div>
-              <div className="text-ourTextLightGray mb-8 text-lg font-semibold">
-                <div>온말 표준데이터에 대한 설명입니다.</div>
+              <div className="text-ourTextGray mb-6 text-lg font-semibold">
+                <div>온말 표준데이터</div>
                 <div>온말 표준데이터에 대한 상세 설명입니다.</div>
               </div>
-              <Link
-                to={'/start-data'}
-                className="bg-ourBlue hover:bg-ourEventBlueDark rounded-lg px-4 py-2 text-white hover:cursor-pointer"
-              >
-                자세히 알아보기
-              </Link>
+              <div className="text-lg">
+                <Link
+                  to={'/start-data'}
+                  className="text-ourBlue my-auto hover:cursor-pointer hover:underline"
+                >
+                  자세히 알아보기
+                </Link>
+                <span className="text-ourBlue px-1 py-2 hover:cursor-default">
+                  {'>'}
+                </span>
+              </div>
             </div>
 
             <div className="rounded-lg bg-neutral-950 px-8 py-12 sm:px-16">
               <div className="mb-1 text-2xl font-bold">온말 다국어</div>
-              <div className="text-ourTextLightGray mb-8 text-lg font-semibold">
+              <div className="text-ourTextGray mb-6 text-lg font-semibold">
                 <div>온말 다국어에 대한 설명입니다.</div>
                 <div>온말 다국어에 대한 상세 설명입니다.</div>
               </div>
-              <Link
-                to={'/start-i18n'}
-                className="bg-ourBlue hover:bg-ourEventBlueDark rounded-lg px-4 py-2 text-white hover:cursor-pointer"
-              >
-                자세히 알아보기
-              </Link>
+              <div className="text-lg">
+                <Link
+                  to={'/start-i18n'}
+                  className="text-ourBlue hover:cursor-pointer hover:underline"
+                >
+                  자세히 알아보기
+                </Link>
+                <span className="text-ourBlue p-2 hover:cursor-default">
+                  {'>'}
+                </span>
+              </div>
             </div>
           </div>
         </div>
